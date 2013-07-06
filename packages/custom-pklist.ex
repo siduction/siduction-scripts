bea98563d8a354af6ecbe3b865fa9d442542bbe6
Ovidiu Angelescu <convbsd@siduction.net>
2013-07-06 19:39:28 +0300

    added addpkg script for adding a siduction- or a custom pkglist over an installed system

---
+++ packages/custom-pklist.ex
@@ -0,0 +1,21 @@
+# Please all comments should begin with #
+# packages = """ and """ at the end of pakages lists 
+# are mandatory
+
+# Desc
+# Add descrition here
+#
+
+packages = """
+    # Please add a package name  on each line as follows
+    # of course without comments :-)
+    # package1
+    # package2
+"""
+
+# Copy custom-pklist.ex in same directory as custom-pklist
+# and edit to add desired packages
+
+###
+# Thank you
+###
