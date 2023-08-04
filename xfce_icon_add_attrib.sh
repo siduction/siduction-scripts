338095789292da631108c09df24999cd9d6e31da
Axel Konrad <emma-konrad@web.de>
2023-08-04 22:17:18 +0200

    Metadata for siduction icons

---
+++ xfce_icon_add_attrib.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Setzen des Atributes metadata::xfce-exe-checksum
+#
+
+cd /home/$(echo $USER)/Desktop/
+gio set -t string "siduction-manual.desktop" metadata::xfce-exe-checksum "$(sha256sum "siduction-manual.desktop" | awk '{print $1}')"
+gio set -t string "siduction-irc.desktop" metadata::xfce-exe-checksum "$(sha256sum "siduction-irc.desktop" | awk '{print $1}')"
+gio set -t string "chroot-helper.desktop" metadata::xfce-exe-checksum "$(sha256sum "chroot-helper.desktop" | awk '{print $1}')" 2>/dev/null
+gio set -t string "calamares.desktop" metadata::xfce-exe-checksum "$(sha256sum "calamares.desktop" | awk '{print $1}')" 2>/dev/null
+rm /home/$(echo $USER)/.config/autostart/Metadata.desktop
+exit 0
