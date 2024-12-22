c946627d45fb9bdf27ebba44519cd15523d4dc7d
Torsten Wohlfarth <towo@siduction.org>
2024-12-22 20:20:21 +0100

    add wallpaper hack for xfce

--- xfce_icon_add_attrib.sh
+++ xfce_icon_add_attrib.sh
@@ -8,5 +8,10 @@ gio set -t string "siduction-manual.desktop" metadata::xfce-exe-checksum "$(sha2
 gio set -t string "siduction-irc.desktop" metadata::xfce-exe-checksum "$(sha256sum "siduction-irc.desktop" | awk '{print $1}')"
 gio set -t string "chroot-helper.desktop" metadata::xfce-exe-checksum "$(sha256sum "chroot-helper.desktop" | awk '{print $1}')" 2>/dev/null
 gio set -t string "calamares.desktop" metadata::xfce-exe-checksum "$(sha256sum "calamares.desktop" | awk '{print $1}')" 2>/dev/null
+sleep 1
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor$(xrandr | grep -m1 connected | awk '{print $1}')/workspace0/last-image -s /usr/share/wallpapers/shine-on-xmas.png --create -t string
+xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-filesystem -s false --create -t bool 
+xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-removable -s false --create -t bool
+
 rm /home/${USER}/.config/autostart/Metadata.desktop
 exit 0
