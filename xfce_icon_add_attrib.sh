afbfd7aaac5989af7af39da98cba316dadf42eb6
Torsten Wohlfarth <towo@siduction.org>
2024-12-23 15:01:55 +0100

    fix xfce wallpaper

--- xfce_icon_add_attrib.sh
+++ xfce_icon_add_attrib.sh
@@ -10,6 +10,8 @@ gio set -t string "chroot-helper.desktop" metadata::xfce-exe-checksum "$(sha256s
 gio set -t string "calamares.desktop" metadata::xfce-exe-checksum "$(sha256sum "calamares.desktop" | awk '{print $1}')" 2>/dev/null
 sleep 1
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor$(xrandr | grep -m1 connected | awk '{print $1}')/workspace0/last-image -s /usr/share/wallpapers/shine-on-xmas.png --create -t string
+xfconf-query -c xfce4-desktop -p /backdrop/single-workspace-mode -s true --create -t bool
+xfconf-query -c xfce4-desktop -p /backdrop/single-workspace-number -s 0 --create -t int
 xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-filesystem -s false --create -t bool 
 xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-removable -s false --create -t bool
 
